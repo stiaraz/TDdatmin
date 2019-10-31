759b6d1b53154899fcdad85358ea6aabf05e22da
anaazqyh <naharyana57@gmail.com>
2019-10-31 10:50:59 +0700

    cfs, ltsa, grid, svm

--- code_kc2.py
+++ code_kc2.py
@@ -5,19 +5,12 @@ Created on Mon Oct 28 20:00:06 2019
 @author: SYAVIRA TIARA Z
 """
 
-import numpy as np
 import pandas as pd
-from sklearn.datasets import make_classification
 from collections import Counter
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn.utils import resample
 from imblearn.combine import SMOTETomek
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.decomposition import PCA
 from sklearn.manifold import LocallyLinearEmbedding
-
+from imblearn.over_sampling import SMOTE
 dataset= pd.read_csv('dataset/kc2.csv', skiprows = range(0,359), header = None)
 
 kelas= dataset[21]
@@ -25,25 +18,44 @@ print('Before: Class{}'. format(Counter(kelas)))
 df = dataset.copy() #train itu yg X tanpa class
 del df[21]
 
-x_train, x_test, y_train, y_test = train_test_split(df, kelas, test_size= .1, random_state=10)
+#normalization
 
-#PCA
-#from sklearn.preprocessing import StandardScaler
-#standardizedData = StandardScaler().fit_transform(df_res)
-#pca = PCA(n_components=2)
-#df_res = pca.fit_transform(X = standardizedData)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+df=scaler.fit_transform(df)
 
-#balancing data
+#oversampling
+
 sm = SMOTETomek()
 df_resm, kelas_res = sm.fit_sample(df, kelas)
-#df_res_vis = pca.transform(df_resm)
+
 print('After: Class{}'. format(Counter(kelas_res)))
+
+#correlation
+
+df_resm=pd.DataFrame(df_resm)
+
+import seaborn as sns
+import numpy as np
+corr = df_resm.corr()
+sns.heatmap(corr)
+
+columns = np.full((corr.shape[0],), True, dtype=bool)
+for i in range(corr.shape[0]):
+    for j in range(i+1, corr.shape[0]):
+        if corr.iloc[i,j] >= 0.9:
+            if columns[j]:
+                columns[j] = False
+selected_columns = df_resm.columns[columns]
+df_resm = df_resm[selected_columns]
+
 ##plotting imbalance
+
 besar= dataset.groupby(kelas).size()
 besar=list(besar)
 koor_x=['no', 'yes']
 koor_y= besar
-
+ 
 kelas_res= list(kelas_res)
 
 valp= kelas_res.count("no")
@@ -59,6 +71,17 @@ plt.ylabel('value')
 plt.legend()
 plt.show()
 
-embedding = LocallyLinearEmbedding(n_components=5, method='ltsa')
-# method='hessian', eigen_solver='dense'
+#ltsa
+
+embedding = LocallyLinearEmbedding(method='ltsa',eigen_solver='dense', n_components=5)
 X_transformed = embedding.fit_transform(df_resm)
+
+#svm and grid search
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+parameters = {'C':[1, 10]}
+svm = SVC(kernel='rbf',gamma='auto')
+clf = GridSearchCV(svm, parameters, cv=10)
+clf.fit(X_transformed, kelas_res)
+print(clf.best_score_)
