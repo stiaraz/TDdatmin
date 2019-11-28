55aa46453e2e5a4cd3e462b99650db742bfc835e
anaazqyh <naharyana57@gmail.com>
2019-11-28 13:14:02 +0700

    update kc2

--- code_kc2.py
+++ code_kc2.py
@@ -72,30 +72,50 @@ df_resm = df_resm[selected_columns]
 
 #ltsa
 
-embedding = LocallyLinearEmbedding(method='ltsa',eigen_solver='dense', n_components=5)
-X_transformed = embedding.fit_transform(df_resm)
+#embedding = LocallyLinearEmbedding(method='ltsa',eigen_solver='dense', n_components=5)
+#X_transformed = embedding.fit_transform(df_resm)
 
 #svm and grid search
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-parameters = {'C':[1, 10]}
-svm = SVC(kernel='rbf',gamma='auto')
+parameters = {'C':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]}
+svm = SVC(kernel='rbf',gamma='10')
 clf = GridSearchCV(svm, parameters, cv=10)
-clf.fit(X_transformed, kelas_res)
+clf.fit(df_resm, kelas_res)
+
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print()
+print("Grid scores on development set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+print()
+
 print(clf.best_score_)
 
 bestmodel = clf.best_estimator_
 
+from sklearn.model_selection import cross_validate
+scoring = ['accuracy','precision_macro', 'recall_macro' ]
+scores = cross_validate(bestmodel, df_resm, kelas_res, cv=10, scoring =scoring)
 
-y_pred = bestmodel.predict(X_transformed)
+print("Accuracy: %0.4f " % (scores['test_accuracy'].mean()))
+print("RECALL: %0.4f " % (scores['test_precision_macro'].mean()))
+print("PRESISI: %0.4f )" % (scores['test_recall_macro'].mean()))
 
+
+y_pred = bestmodel.predict(df_resm)
 
 from sklearn.metrics import classification_report
 y_true_svm = kelas_res
 y_pred_svm = y_pred
 print(classification_report(y_true_svm, y_pred_svm))
-
 
 
 from sklearn import metrics
